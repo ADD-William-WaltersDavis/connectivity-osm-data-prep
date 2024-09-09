b8419233594795973ecf084fb980940cea399012
ADD-William-WaltersDavis <william.waltersdavis@dft.gov.uk>
2024-09-09 10:45:44 +0100

    feat: calculate simplified values for nodes based on location of destinations

--- destinations_from_pbf/src/lib.rs
+++ destinations_from_pbf/src/lib.rs
@@ -1,5 +1,9 @@
-use geo::{Point, Geometry};
-use serde::Serialize;
+use anyhow::Result;
+use fs_err::File;
+use geo::{Coord, Geometry, Point};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::io::{BufWriter, Write};
 
 #[derive(Serialize)]
 pub struct Destination {
@@ -9,4 +13,31 @@ pub struct Destination {
     pub subpurpose: String,
     pub geometry: Geometry,
     pub centroid: Point,
+}
+
+// #[derive(Deserialize)]
+// pub struct Nodes (
+//     pub HashMap<usize, Coord>
+// );
+
+pub fn read_nodes(mode: &str) -> Result<HashMap<usize, Coord>> {
+    let inpath = format!("../data/{}_nodes.json", mode);
+    let file = File::open(inpath)?;
+    let reader = std::io::BufReader::new(file);
+    let nodes: HashMap<usize, Coord> = serde_json::from_reader(reader)?;
+    Ok(nodes)
+}
+
+pub fn write_json_file<T: Serialize>(
+    file_name: String,
+    output_directory: &str,
+    data: T,
+) -> Result<()> {
+    let path = format!("{output_directory}/{file_name}.json");
+    println!("Writing to {path}");
+    let file = File::create(path)?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &data)?;
+    writer.flush()?;
+    Ok(())
 }
--- destinations_from_pbf/src/main.rs
+++ destinations_from_pbf/src/main.rs
@@ -1,34 +1,27 @@
 mod destinations;
+mod values;
 
 use anyhow::Result;
-use fs_err::File;
-use serde::Serialize;
-use std::io::{BufWriter, Write};
+use destinations_from_pbf::write_json_file;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
         panic!("Call with the input path to an osm.pbf and output directory");
     }
-    run(&args[1], &args[2]).unwrap();
+    for mode in ["walk", "cycling"].iter() {
+        run(&args[1], &args[2], mode).unwrap();
+    }
 }
 
-fn run(osm_path: &str, output_directory: &str) -> Result<()> {
+fn run(osm_path: &str, output_directory: &str, mode: &str) -> Result<()> {
     let destinations = destinations::process(osm_path)?;
+    let graph_values = values::process(&destinations, mode)?;
     write_json_file("destinations".to_string(), output_directory, destinations)?;
-    Ok(())
-}
-
-pub fn write_json_file<T: Serialize>(
-    file_name: String,
-    output_directory: &str,
-    data: T,
-) -> Result<()> {
-    let path = format!("{output_directory}/{file_name}.json");
-    println!("Writing to {path}");
-    let file = File::create(path)?;
-    let mut writer = BufWriter::new(file);
-    serde_json::to_writer(&mut writer, &data)?;
-    writer.flush()?;
+    write_json_file(
+        format!("{}_graph_values", mode),
+        output_directory,
+        graph_values,
+    )?;
     Ok(())
 }
