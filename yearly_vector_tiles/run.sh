e137c90434518a436f3e210436c99a6574d0b055
ADD-William-WaltersDavis <william.waltersdavis@dft.gov.uk>
2024-10-30 13:09:42 +0000

    feat: iterate over all years and remove local tmp files

--- yearly_vector_tiles/run.sh
+++ yearly_vector_tiles/run.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-for year in 22 23 24; do 
+for year in 14 15 16 17 18 19 20 21 22 23 24; do 
 
     mkdir -p input
-    wget https://download.geofabrik.de/europe/england-${year}0101.osm.pbf -O input/england-${year}0101.osm.pbf
+    wget https://download.geofabrik.de/europe/united-kingdom/england-${year}0101.osm.pbf -O input/england-${year}0101.osm.pbf
 
     for mode in "walk" "cycling"; do
         mkdir -p tmp
@@ -19,10 +19,14 @@ for year in 22 23 24; do
             -zg \
             --drop-densest-as-needed \
             --extend-zooms-if-still-dropping \
-            -o england-${year}-${mode}.mbtiles
+            -o tmp/england-${year}-${mode}.mbtiles
 
-        time ../../pmtiles convert england-${year}-${mode}.mbtiles england-20${year}-${mode}.pmtiles
+        time ../../pmtiles convert tmp/england-${year}-${mode}.mbtiles tmp/england-20${year}-${mode}.pmtiles
 
-        time gsutil -m cp england-20${year}-${mode}.pmtiles gs://very-nice-tiles-bucket/yearly-osm/england/20${year}-${mode}.pmtiles
+        time gsutil -m cp tmp/england-20${year}-${mode}.pmtiles gs://very-nice-tiles-bucket/yearly-osm/england/20${year}-${mode}.pmtiles
+        
+        rm tmp/england-${year}-${mode}.geojson
+        rm tmp/england-${year}-${mode}.mbtiles
+        rm tmp/england-20${year}-${mode}.pmtiles
     done
 done
